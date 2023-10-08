1a9ba43c86e6a281243e2e4acef487b80f84e768
Muhammad Iqbal Hussain <iqbalhussain931@gmail.com>
2023-10-08 12:01:36 -0500

    Added styled-component and storybook.

---
+++ app/routes/login.tsx
@@ -0,0 +1,63 @@
+// import { Link, useLoaderData } from "@remix-run/react";
+import type { ActionFunction, LoaderFunctionArgs } from '@remix-run/node';
+import { Form, Link } from "@remix-run/react";
+import { authenticator } from "utils/auth.server";
+
+const loader = async ({ request }: LoaderFunctionArgs) => {
+    return await authenticator.isAuthenticated(request, {
+        successRedirect: "/resolutions",
+    });
+}
+
+const action: ActionFunction = async ({ request }) => {
+    return authenticator.authenticate("form", request, {
+        successRedirect: "/resolutions",
+        failureRedirect: "/login",
+    });
+}
+
+const LoginPage = () => {
+    return (
+        <Form method="post" className="p-10 text-center">
+            <h1 className="font-bold text-xl">
+                Welcome! Login to see your resolutions.
+            </h1>
+            <p className="mb-6">
+                Don't have an account?{" "}
+                <Link to="/signup" className="text-blue-500">
+                    Sign up
+                </Link>
+            </p>
+
+            <label className="font-semibold mr-2" htmlFor="email">
+                Email
+            </label>
+            <input
+                className="border-2 rounded-md mr-8 border-gray-600 px-3 py-1"
+                type="email"
+                name="email"
+                id="email"
+            />
+
+            <label className="font-semibold mr-2" htmlFor="password">
+                Password
+            </label>
+            <input
+                className="border-2 rounded-md mr-8 border-gray-600 px-3 py-1"
+                type="password"
+                name="password"
+                id="password"
+            />
+
+            <button
+                type="submit"
+                className="bg-blue-500 text-white py-1 px-3 rounded-md font-semibold"
+            >
+                Login
+            </button>
+        </Form>
+    )
+}
+
+export default LoginPage
+export { action, loader }
